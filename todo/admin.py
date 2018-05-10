3eb9d9780cdb7f70b98ccfa112b30993b2c262ff
linda314014 <l940026l@gmail.com>
2018-05-10 18:13:39 +0800

    0510

---
+++ todo/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from.models import Todo, Tag
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status','creator')
+    list_filter = ('status' ,)
+    search_fields = ('title','creator__username')
+
+
+admin.site.register(Todo ,TodoAdmin)
+admin.site.register(Tag)
